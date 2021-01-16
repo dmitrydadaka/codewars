17141b9c24a61d58d859208afed36d9aecea3f0c
User <belpc2012@gmail.com>
2021-01-16 16:03:35 +0300

    task

---
+++ Smallest unused ID.js
@@ -0,0 +1,8 @@
+function nextId(ids){
+    for (i = 0; i < ids.length; i++) { 
+      if (ids.indexOf(i) == -1){
+        return i;
+      }
+    }
+    return ids.length;
+  }
---
+++ Smallest unused ID2.js
@@ -0,0 +1,6 @@
+function nextId(ids){
+    const used = new Set(ids);
+    for (let i = 0; i <= ids.length; i++) {
+      if (!used.has(i)) return i;
+    }
+  }
